07df80d78162b90e3486004486fc55362517b6a0
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2025-10-14 20:27:43 +0200

    first

---
+++ src/pages/quizzModal.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+//https://dribbble.com/shots/24364665-slothUI-World-s-Laziest-Design-System-E-Learning-Quiz-UIUX
+//https://dribbble.com/shots/24364687-slothUI-World-s-Laziest-Design-System-E-Learning-Quiz-Dark
+</script>
+
+<template>
+    <div class="flex flex-col">
+        
+        <h1>
+            Quizz
+        </h1>
+        <div class="bg-blue-500 w-215 h-28 m-2 mb-4 rounded-lg">01</div>
+        <div class="grid grid-cols-2 gap-2">
+        <button class="bg-blue-500 hover:bg-cyan-500 cursor-pointer w-104 h-16 m-2 rounded-lg">02</button>
+        <button class="bg-blue-500 hover:bg-cyan-500 cursor-pointer w-104 h-16 m-2 rounded-lg">03</button>
+        <button class="bg-blue-500 hover:bg-cyan-500 cursor-pointer w-104 h-16 m-2 rounded-lg">02</button>
+        <button class="bg-blue-500 hover:bg-cyan-500 cursor-pointer w-104 h-16 m-2 rounded-lg">03</button>
+       </div>
+    </div>
+
+</template>
+
+<style scoped></style>
